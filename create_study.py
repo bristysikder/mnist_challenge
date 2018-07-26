af02fd89cac2adc35dcc5325ea022af5f3020f79
Bristy Sikder <bristy.sikder.work@gmail.com>
2018-07-26 18:40:56 -0400

    Edit bugs in Study.

--- create_study.py
+++ create_study.py
@@ -8,9 +8,9 @@ from madry_tools.core import datatypes
 
 # a commit to a version of this repository with a stable version of the main
 # script
-GIT_COMMIT = 'https://github.com/bristysikder/mnist_challenge/commit/8f167768bf09f22883a1c9fc464c0ee1692c0cd8'
+GIT_COMMIT = 'https://github.com/bristysikder/mnist_challenge/commit/81e75b62736608239670598fe4060e0a29042905'
 MAIN_FILE_NAME = 'nat_train.py'
-FILES_TO_ARCHIVE = ['job_parameters.json', 'config.py', 'job_result.json']
+FILES_TO_ARCHIVE = ['job_parameters.json']
 
 ### CREATE STUDY
 
@@ -18,22 +18,24 @@ FILES_TO_ARCHIVE = ['job_parameters.json', 'config.py', 'job_result.json']
 client = RPCClient()
 username = getpass.getuser()
 
-study_name = 'bristy_mnist_compression'
+study_name = 'mnist_compression_simple_mlp'
 study_id, error_msg = client.create_study(study_name)
 
 if error_msg is not None:
     print(error_msg)
     sys.exit(1)
 
-train_steps = [i * 5000 for i in range(200)]
-
+# Epsilon from Algorith #1 of Sanjeev Aroras paper
+epsilons = [0.5, 0.25, 0.1, 0.05]
 
 ### CREATE JOBS
 jobs = []
 prio = 200
-k = 10
-for t in train_steps:
-    params = {'max_num_training_steps': t}
+
+for i, eps in enumerate( epsilons):
+    model_dir = ('models/compression_%d') % i
+    params = {'c_eps': eps, 'nu': eps, 
+              'model_dir': model_dir }
     parameter_json = json.dumps(params)
     job_info = datatypes.JobCreationInfo(
         git_commit=GIT_COMMIT,
